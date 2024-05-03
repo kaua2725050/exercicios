1c790b6b057e38b8942443e051a64aaebd7458ab
KAUA PEREIRA <54439995837@info.local>
2024-05-03 16:14:57 -0300

    exercicio 2 feito

--- ex1.js
+++ ex1.js
@@ -1,3 +1,8 @@
+//Exercício 1: Verificação de login
+//Escreva um programa que solicita ao usuário um nome de usuário e uma senha. Verifique se o
+//nome de usuário é &quot;admin&quot; e a senha é &quot;senha123&quot;. Exiba uma mensagem indicando se o login
+//foi bem-sucedido ou não.
+
 let username = prompt("Digite um nome de usuário:")
 let senha = prompt("Digite a senha:")
 
